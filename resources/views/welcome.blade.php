33851e18b11cb7ec4f2bf5ef2fc33973810b48ed
Jay-Are Ocero <199ocero@gmail.com>
2020-11-25 23:16:13 +0800

    messenger plugin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,29 @@
             <a href="https://github.com/botman/botman" target="_blank">GitHub</a>
         </div>
     </div>
+    <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="107697914382323">
+      </div>
 </div>
 </body>
 </html>
